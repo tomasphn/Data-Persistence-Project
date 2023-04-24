5ee5515e71a532809dd0747dd417ac5446fc8ad6
Tomas Palpallatoc <tomasphn@gmail.com>
2023-04-24 14:23:21 -0700

    add persistence for high scores between sessions

--- Assets/Scripts/HighScoreManager.cs
+++ Assets/Scripts/HighScoreManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class HighScoreManager : MonoBehaviour
 {
@@ -19,21 +20,43 @@ public class HighScoreManager : MonoBehaviour
 
         Instance = this;
         DontDestroyOnLoad(gameObject);
+        LoadHighScore();
     }
 
     public void UpdateHighScore(int newScore)
     {
       HighScore = newScore;
       HighScoreName = CurrentPlayerName;
+      SaveHighScore();
     }
 
-    public void LoadHighScore()
+    class SaveData
     {
-        // load high score
+        public string HighScoreName;
+        public int HighScore;
     }
 
     public void SaveHighScore()
     {
         // save high score
+        SaveData data = new SaveData();
+        data.HighScore = HighScore;
+        data.HighScoreName = HighScoreName;
+        
+        string json = JsonUtility.ToJson(data);
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
     }
+    public void LoadHighScore()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+            HighScore = data.HighScore;
+            HighScoreName = data.HighScoreName;
+        }
+    }
+
+
 }
